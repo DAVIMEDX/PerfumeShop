b8ddb90f728accb0539f81a4b0ad6e471d73d0a0
Davi Medeiros <97980463+DAVIMEDX@users.noreply.github.com>
2025-07-15 01:13:41 -0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,8 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# String de conexão PostgreSQL
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:medx4@localhost:5432/Projeto_ES"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
